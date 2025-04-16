c10b11b5e7ab706ebab2aed7270c884ccd3ca4b9
S Rajesh <113459104+enstenr@users.noreply.github.com>
2025-04-16 14:46:32 +0000

    modified uvicorn

--- startup.sh
+++ startup.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
 echo "Starting Uvicorn on port $PORT..."
-/opt/hostedtoolcache/Python/3.11.11/x64/bin/uvicorn main:app --host 0.0.0.0 --port $PORT
+python -m uvicorn main:app --host 0.0.0.0
