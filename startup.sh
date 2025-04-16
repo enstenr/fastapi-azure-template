8439636ddcae8498e6ada0c19fdbc594ec9aba78
S Rajesh <113459104+enstenr@users.noreply.github.com>
2025-04-16 14:30:34 +0000

    updating the prot

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 echo '#!/bin/sh' > startup.sh
-echo 'uvicorn main:app --host 0.0.0.0 --port 8000' >> startup.sh
+echo 'uvicorn main:app --host 0.0.0.0 --port $PORT' >> startup.sh
 chmod +x startup.sh
