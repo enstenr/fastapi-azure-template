74275f67a25eb2ed6ef4e49d367b8f4788b541f9
S Rajesh <113459104+enstenr@users.noreply.github.com>
2025-04-23 06:32:04 +0000

    UPDATING MESSAGE

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ async def hello(request: Request, name: str = Form(...)):
 
 @app.get("/status", response_class=JSONResponse)
 async def get_status():
-    return {"status": "ok", "message": "API is running"}
-    
+    return {"status": "ok", "message": "API is running  "}
+
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
 
